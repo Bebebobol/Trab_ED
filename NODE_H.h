2490edace1442a2ebfbd2e7c9f647d624be83e99
Lorenzo Alencastro <lorenzo1711@hotmail.co.uk>
2023-06-28 12:51:09 -0300

    Adding Node header file

---
+++ NODE_H.h
@@ -0,0 +1,26 @@
+#ifndef NODE_H
+#define NODE_H
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* createNode(int value);
+
+Node* InsertNode(Node* head, int value);
+
+void bubbleSort(int arr[]);
+
+void printInorder(Node* head);
+
+int countNodes(Node* root);
+
+int MorrisTraversal(Node* head, int num)
+
+Node* Texfile();  // Matheus mude o nome da função de acordo com o seu módulo
+
+
+
+#endif
